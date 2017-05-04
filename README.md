f0730aa8ad03e6ffec29d4fb7f70f921d71c9181
gund <malkevich.alex@gmail.com>
2017-05-04 20:11:55 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Game Of Life
+
+> The Great Game of Life written in Angular
+
+Teck stack: Angular 4, RxJs, NgRx, D3.
---
+++ src/app/app-nav-views.ts
@@ -0,0 +1,18 @@
+export const views: Object[] = [
+  {
+    name: 'Dashboard',
+    link: ['']
+  },
+  {
+    name: 'Lazy',
+    link: ['lazy']
+  },
+    {
+    name: 'Sync',
+    link: ['sync']
+  },
+  {
+    name: 'Bad Link',
+    link: ['wronglink']
+  }
+];
